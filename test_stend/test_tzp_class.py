1fc86fa538ff1de5e3d6200dc1278bd5063ec1d3
irik_safin <osiris365@gmail.com>
2022-02-23 17:41:01 +0500

    избавился от конкатенации опрос входов переведен в статик

--- test_stend/test_tzp_class.py
+++ test_stend/test_tzp_class.py
@@ -84,7 +84,7 @@ class TestTZP(object):
         min_volt = 0.6 * meas_volt_ust
         max_volt = 1.1 * meas_volt_ust
         meas_volt = read_mb.read_analog()
-        fault.debug_msg("напряжение \t" + str(meas_volt), 3)
+        fault.debug_msg(f'напряжение \t{meas_volt}', 3)
         if min_volt <= meas_volt <= max_volt:
             fault.debug_msg("напряжение соответствует", 3)
             pass
@@ -169,15 +169,15 @@ class TestTZP(object):
         mysql_conn.mysql_ins_result('идет тест 3', '3')
         k = 0
         for i in list_ust:
-            mysql_conn.mysql_ins_result('проверка уставки ' + str(list_ust_num[k]), '3')
-            msg_5 = ("Установите регулятор уставок на блоке в положение \t" + str(list_ust_num[k]))
+            mysql_conn.mysql_ins_result(f'проверка уставки {list_ust_num[k]}', '3')
+            msg_5 = (f'Установите регулятор уставок на блоке в положение \t{list_ust_num[k]}')
             msg_result = my_msg_2(msg_5)
             if msg_result == 0:
                 pass
             elif msg_result == 1:
                 return False
             elif msg_result == 2:
-                mysql_conn.mysql_add_message('уставка ' + str(list_ust_num[k]) + ' пропущена')
+                mysql_conn.mysql_add_message(f'уставка {list_ust_num[k]} пропущена')
                 list_delta_percent.append('пропущена')
                 list_delta_t.append('пропущена')
                 k += 1
@@ -195,7 +195,7 @@ class TestTZP(object):
                 # Включение вторичного главного контакта	KL63 - ВКЛ	DQ5:1B - ВКЛ
                 # Запуск таймера происходит по условию замыкания DI.b0 (контакт реле KL63)
                 # Остановка таймера происходит по условию размыкания DI.a5 T1[i]
-                fault.debug_msg("напряжение соответствует " + str(meas_volt), 3)
+                fault.debug_msg(f'напряжение соответствует {meas_volt}', 3)
                 ctrl_kl.ctrl_relay('KL63', True)
                 in_b0, in_b1 = self.__inputs_b()
                 while in_b0 == False:
@@ -206,18 +206,16 @@ class TestTZP(object):
                 while in_a5 == True and sub_timer <= 370:
                     sleep(0.2)
                     sub_timer = time() - start_timer
-                    fault.debug_msg("времени прошло " + str(sub_timer), 2)
+                    fault.debug_msg(f'времени прошло {sub_timer}', 2)
                     in_a1, in_a5 = self.__inputs_a()
                 stop_timer = time()
                 ctrl_kl.ctrl_relay('KL63', False)
                 calc_delta_t = stop_timer - start_timer
                 reset.stop_procedure_3()
-                fault.debug_msg("тест 3 delta t " + str(calc_delta_t), 4)
+                fault.debug_msg(f'тест 3 delta t: {calc_delta_t}', 4)
                 list_delta_t.append(calc_delta_t)
-                mysql_conn.mysql_add_message('уставка ' + str(list_ust_num[k]) + ' дельта t: '
-                                             + str(round(calc_delta_t, 0)))
-                mysql_conn.mysql_add_message('уставка ' + str(list_ust_num[k]) + ' дельта %: '
-                                             + str(round(calc_delta_percent, 0)))
+                mysql_conn.mysql_add_message(f'уставка {list_ust_num[k]} дельта t: {calc_delta_t:.0f}')
+                mysql_conn.mysql_add_message(f'уставка {list_ust_num[k]} дельта %: {calc_delta_percent:.0f}')
                 in_a1, in_a5 = self.__inputs_a()
                 if calc_delta_t <= 360 and in_a1 == True and in_a5 == False:
                     # Если в период времени до 6 минут входа DI.A1, DI.A5 занимают
@@ -280,12 +278,14 @@ class TestTZP(object):
             mysql_conn.mysql_error(285)
             return False
     
-    def __inputs_a(self):
+    @staticmethod
+    def __inputs_a():
         in_a1 = read_mb.read_discrete(1)
         in_a5 = read_mb.read_discrete(5)
         return in_a1, in_a5
     
-    def __inputs_b(self):
+    @staticmethod
+    def __inputs_b():
         in_b0 = read_mb.read_discrete(8)
         in_b1 = read_mb.read_discrete(9)
         return in_b0, in_b1
